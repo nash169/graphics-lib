b0d827a07dcb2fab7581ab1a922566e008ab7c10
Bernardo Fichera <bernardo.fichera@gmail.com>
2020-04-10 17:51:41 +0200

    Added some resource; loading from multiple files not working yet.

--- src/examples/test.cpp
+++ src/examples/test.cpp
@@ -11,7 +11,8 @@ int main(int argc, char** argv)
     // const Trade::MeshData3D cube = Primitives::cubeSolid();
     // app.addPrimitive(cube, Matrix4::translation(Vector3::xAxis(-3.0f)), 0xff0000_rgbf);
     // app.addPrimitive(cube, Matrix4::translation(Vector3::xAxis(3.0f)), 0x00ff00_rgbf);
-    app.importScene("rsc/drone.stl");
+    app.importScene("rsc/franka/link0.stl");
+    app.importScene("rsc/franka/link1.stl");
 
     return app.exec();
 }
