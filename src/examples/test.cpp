1f5e5e961110cea05a27dca9b6f2bcde2f131bb9
bernardo <bernardo.fichera@gmail.com>
2021-04-06 17:23:47 +0200

    Added (commented) object axis drawing via DebugTools.

--- src/examples/test.cpp
+++ src/examples/test.cpp
@@ -2,38 +2,24 @@
 
 #include <magnum_dynamics/MagnumApp.hpp>
 
+// #include <Magnum/DebugTools/ObjectRenderer.h>
+// #include <Magnum/DebugTools/ResourceManager.h>
+
 using namespace magnum_dynamics;
 
 int main(int argc, char** argv)
 {
     MagnumApp app({argc, argv});
 
-    // Eigen::Vector3f a{1.0f, 2.0f, 3.0f};
-    // Vector3 b(a);
-
-    app.add("cube", "", Matrix4::scaling({0.5f, 0.5f, 0.5f}) * Matrix4::translation({0.0f, -2.0f, 0.0f}), 0xff0000_rgbf);
+    // app.add("cube", "", Matrix4::scaling({0.5f, 0.5f, 0.5f}) * Matrix4::translation({0.0f, -2.0f, 0.0f}), 0xff0000_rgbf);
     // app.add("cube", "", Matrix4::scaling({0.5f, 0.5f, 0.5f}) * Matrix4::translation({0.0f, 2.0f, 0.0f}), 0x00ff00_rgbf);
 
-    auto* obj = app.manipulator().children().last();
+    app.add("rsc/franka/link1.dae");
 
-    app.add("rsc/franka/link0.dae");
+    // DebugTools::ResourceManager manager;
+    // manager.set("my", DebugTools::ObjectRendererOptions{}.setSize(0.3f));
 
-    int index = 0;
-    for (Object3D* child = obj->nextSibling(); child; child = child->nextSibling()) {
-        std::cout << index << std::endl;
-        index++;
-    }
-
-    // auto* obj1 = new Object3D(&app.manipulator());
-
-    // auto* obj2 = new Object3D(obj1);
-
-    // auto* obj3 = new Object3D(obj2);
-
-    // for (Object3D& child : app.manipulator().children()) {
-    //     std::cout << index << std::endl;
-    //     index++;
-    // }
+    // auto debug = new DebugTools::ObjectRenderer3D{manager, *app.manipulator().children().last(), my, &app.drawables()};
 
     return app.exec();
 }
